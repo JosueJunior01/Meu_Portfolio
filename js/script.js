3f8b2ae5e1a2e76821f91f6bdef300cb3affc37d
Josué Júnior <99128300+JosueJunior01@users.noreply.github.com>
2023-02-25 23:04:01 -0300

    1.10
    Animação com JavaScript E TypeScript

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ const icon = document.querySelector(".iconDiaNoite");
 icon.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
+
+// Efeito de escrita
+new TypeIt(".animacao", {
+    speed: 50,
+    waitUntilVisible: true,
+})
+    .type("Desemvol", { delay: 900 })
+    .move(-3)
+    .delete(1)
+    .type("n")
+    .move(null, { to: "END" })
+    .type("vedoor")
+    .pause(300)
+    .delete(2)
+    .type("r")
+    .move(null, { to: "END" })
+    .type(" Frontend.")
+    .delete(4, { delay:300})
+    .type(" end.")
+    .move(null, { to: "END"})
+    .pause(500)
+    .go();
